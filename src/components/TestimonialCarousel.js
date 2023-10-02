775ba479dd5278c454fdd5893fdeb409eb41c648
Nitin Singh <143168728+nitindev07@users.noreply.github.com>
2023-10-02 16:25:28 +0530

    changes

--- src/components/TestimonialCarousel.js
+++ src/components/TestimonialCarousel.js
@@ -9,7 +9,7 @@ const TestimonialCarousel = ({slider}) => {
       {slider.map((item,idx)=>{
         const{message,image,name,occupation}=item
         return <div key={idx} className='w-full'>
-          <div className='flex flex-col items-center justify-center lg:max-w-[600px] max-w-[250px] mx-auto px-4'>
+          <div className='flex flex-col items-center justify-center lg:max-w-[600px] max-w-[220px] mx-auto px-4'>
             <p className='text-[20px] text-white font-medium mb-10'>{message}</p>
            <div> <img className='mb-4' src={image} alt="" /></div>
             <h5 className='text-2xl text-accent font-medium'>{name}</h5>
